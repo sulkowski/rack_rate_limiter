e66f60f7c670a644ed2ef3e67e90c1330cf00948
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-04 22:06:17 +0200

    [#3] - Add tests for `X-RateLimit-Remaining` header; Make minor improvements in other tests;

--- spec/rack_rate_limiter_spec.rb
+++ spec/rack_rate_limiter_spec.rb
@@ -15,20 +15,49 @@ describe Rack::RateLimiter do
   end
 
   describe 'X-RateLimit-Limit' do
-    it 'has the X-RateLimit-Limit in the header' do
-      get '/'
-      expect(last_response.header).to include('X-RateLimit-Limit')
+    describe 'default value of the `X-RateLimit-Limit`' do
+      before(:each) { get '/' }
+      it 'has the `X-RateLimit-Limit` in the header' do
+        expect(last_response.header).to include('X-RateLimit-Limit')
+      end
+
+      it 'has a default value of the `X-RateLimit-Limit`' do
+        expect(last_response.header['X-RateLimit-Limit']).to eq(60)
+      end
     end
 
-    it 'has a default value of the `X-RateLimit-Limit` in the header' do
+    describe 'custom value of `X-RateLimit-Limit`' do
+      before(:each) do
+        @rate_limiter_options = { limit: 40 }
+        get '/'
+      end
+
+      it 'has a custom value of the `X-RateLimit-Limit`' do
+        expect(last_response.header['X-RateLimit-Limit']).to eq(40)
+      end
+    end
+  end
+
+  describe 'X-RateLimit-Remaining' do
+    it 'has the `X-RateLimit-Remaining` in the header' do
       get '/'
-      expect(last_response.header['X-RateLimit-Limit']).to eq(60)
+      expect(last_response.header).to include('X-RateLimit-Remaining')
     end
 
-    it 'has a custom value of the `X-RateLimit-Limit` in the header' do
-      @rate_limiter_options = { limit: 40 }
-      get '/'
-      expect(last_response.header['X-RateLimit-Limit']).to eq(40)
+    it 'deacreases the `X-RateLimit-Remaining` with each request' do
+      (1..20).each do |request_number|
+        get '/'
+        expect(last_response.header['X-RateLimit-Remaining']).to eq(60 - request_number)
+      end
+      expect(last_response.header['X-RateLimit-Remaining']).to eq(40)
+    end
+
+    it 'stops decreasing the `X-RateLimit-Remaining` after reaching zero' do
+      59.times { get '/' }
+      expect(last_response.header['X-RateLimit-Remaining']).to eq(1)
+
+      2.times { get '/' }
+      expect(last_response.header['X-RateLimit-Remaining']).to eq(0)
     end
   end
 end
