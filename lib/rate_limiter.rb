ee0c242be1b232aa246a15939ef3d9cd184793c9
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-04 17:30:10 +0200

    [#2] - Make `RateLimit` configurable in the constructor of the `RateLimiter` class

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -2,13 +2,14 @@ require 'rack/rate_limiter/version'
 
 module Rack
   class RateLimiter
-    def initialize(app)
-      @app = app
+    def initialize(app, options ={})
+      @app        = app
+      @rate_limit = options[:limit] || 60
     end
 
     def call(env)
-      status, headers, response = @app.call(env)
-      headers['X-RateLimit-Limit'] = 60
+      status, headers, response    = @app.call(env)
+      headers['X-RateLimit-Limit'] = @rate_limit
 
       [status, headers, response]
     end
