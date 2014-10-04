fea786fddced48ecc7f6e19ad6485df568845880
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-04 15:53:42 +0200

    [#2] - Create a middleware that adds the `X-RateLimit-Limit` with a constant value to the header

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -2,5 +2,15 @@ require 'rack/rate_limiter/version'
 
 module Rack
   class RateLimiter
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      status, headers, response = @app.call(env)
+      headers['X-RateLimit-Limit'] = 60
+
+      [status, headers, response]
+    end
   end
 end
--- test/test_rack_rate_limiter.rb
+++ test/test_rack_rate_limiter.rb
@@ -1,11 +1,12 @@
 require 'minitest/autorun'
 require 'rack/test'
+require 'rate_limiter'
 
 class TestRakeLimiter < MiniTest::Test
   include Rack::Test::Methods
 
   def app
-    lambda { |env| [200, {}, ''] }
+    Rack::RateLimiter.new(lambda { |env| [200, {}, ''] })
   end
 
   def test_successful_response
