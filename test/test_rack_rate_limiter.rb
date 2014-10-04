59923e4d41b0f69bbba748677a6b7d4f181b1fa2
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-04 17:28:22 +0200

    [#2] - Add tests for the default and custom value of the `X-RateLimit-Limit`

--- test/test_rack_rate_limiter.rb
+++ test/test_rack_rate_limiter.rb
@@ -5,8 +5,12 @@ require 'rate_limiter'
 class TestRateLimiter < MiniTest::Test
   include Rack::Test::Methods
 
+  def setup
+    @rate_limiter_options = {}
+  end
+
   def app
-    Rack::RateLimiter.new(lambda { |env| [200, {}, ''] })
+    Rack::RateLimiter.new(lambda { |env| [200, {}, ''] }, @rate_limiter_options)
   end
 
   def test_successful_response
@@ -14,9 +18,20 @@ class TestRateLimiter < MiniTest::Test
     assert last_response.ok?
   end
 
-  def test_existence_and_value_of_x_rate_limit_in_the_header
+  def test_presence_of_x_rate_limit_in_the_header
     get '/'
     assert last_response.header.include?('X-RateLimit-Limit')
     assert_equal 60, last_response.header['X-RateLimit-Limit']
+  end
+
+  def test_default_value_of_x_rate_limit_in_the_header
+    get '/'
+    assert_equal 60, last_response.header['X-RateLimit-Limit']
+  end
+
+  def test_custom_value_of_x_rate_limit_in_the_header
+    @rate_limiter_options = { limit: 40 }
+    get '/'
+    assert_equal 40, last_response.header['X-RateLimit-Limit']
   end
 end
