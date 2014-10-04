781707d9c33888c05fd20252ca049af2f72dd362
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-04 22:08:17 +0200

    [#3] - Add the `X-RateLimit-Remaining` header to the `RateLimiter`

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -2,16 +2,28 @@ require 'rack/rate_limiter/version'
 
 module Rack
   class RateLimiter
+    DEFAULT_RATE_LIMIT = 60
+
     def initialize(app, options ={})
-      @app        = app
-      @rate_limit = options[:limit] || 60
+      @app = app
+
+      @rate_limit         = options[:limit] || DEFAULT_RATE_LIMIT
+      @remaining_requests = @rate_limit
     end
 
     def call(env)
-      status, headers, response    = @app.call(env)
-      headers['X-RateLimit-Limit'] = @rate_limit
+      status, headers, response        = @app.call(env)
+      headers['X-RateLimit-Limit']     = @rate_limit
+      headers['X-RateLimit-Remaining'] = decrease_remaining_request
 
       [status, headers, response]
+    end
+
+    private
+
+    def decrease_remaining_request
+      @rate_limit -= 1 unless @rate_limit.zero?
+      @rate_limit
     end
   end
 end
