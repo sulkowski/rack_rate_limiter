0e85dd6c33a9142474d2324b7728c2517335448c
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-05 22:47:52 +0200

    [#8] - Add the possibility to use built-in or extarnal memory

--- lib/rate_limiter.rb
+++ lib/rate_limiter.rb
@@ -5,24 +5,39 @@ module Rack
   class TooManyRequests < RangeError; end
 
   class RateLimiter
+    class InternalMemory
+      def initialize
+        @memory = Hash.new
+      end
+
+      def get(attribute)
+        @memory[attribute.to_s]
+      end
+
+      def set(attribute, value)
+        @memory[attribute.to_s] = value
+      end
+    end
+
     DEFAULT_RATE_LIMIT = 60
 
     def initialize(app, options ={}, &customization_block)
-      @app = app
+      @memory = set_memory(options[:memory])
 
-      @rate_limit = options[:limit] || DEFAULT_RATE_LIMIT
-      @users      = Hash.new { |hash, key| hash[key] = { remaining_requests: @rate_limit,
-                                                         reset_time: time_after_an_hour(Time.now).to_i } }
-      @customization_block = customization_block
+      @memory.set('app', app)
+      @memory.set('rate_limit', options[:limit] || DEFAULT_RATE_LIMIT)
+      @memory.set('users', Hash.new { |hash, key| hash[key] = { remaining_requests: @memory.get('rate_limit'),
+                                                               reset_time: time_after_an_hour(Time.now).to_i } } )
+      @memory.set('customization_block', customization_block)
     end
 
     def call(env)
-      status, headers, response = @app.call(env)
+      status, headers, response = @memory.get('app').call(env)
 
       if user_id = get_user_id(env)
-        user = update_user_attributes(@users[user_id])
+        user = update_user_attributes(@memory.get('users')[user_id])
 
-        headers['X-RateLimit-Limit']     = @rate_limit
+        headers['X-RateLimit-Limit']     = @memory.get('rate_limit')
         headers['X-RateLimit-Remaining'] = user[:remaining_requests]
         headers['X-RateLimit-Reset']     = user[:reset_time]
       end
@@ -33,6 +48,10 @@ module Rack
     end
 
     private
+
+    def set_memory(memory)
+      @memory = memory.nil? ? InternalMemory.new  : memory
+    end
 
     def decrease_remaining_requests(user)
       raise TooManyRequests if user[:remaining_requests].zero?
@@ -48,8 +67,8 @@ module Rack
     end
 
     def get_user_id(env)
-      if @customization_block
-        user_id = @customization_block.call(env)
+      if  @memory.get('customization_block')
+        user_id = @memory.get('customization_block').call(env)
       else
         user_id = env['REMOTE_ADDR']
       end
