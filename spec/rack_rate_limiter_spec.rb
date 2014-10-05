c98227b832713e48fea02c199c3e3ede0e3928f8
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2014-10-05 21:10:14 +0200

    [#7] - Add tests for custom user detection

--- spec/rack_rate_limiter_spec.rb
+++ spec/rack_rate_limiter_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe Rack::RateLimiter do
   let(:rate_limiter_options) { {} }
+  let(:rake_limiter_customization_block) {}
 
   def app
-    Rack::RateLimiter.new(lambda { |env| [200, {}, ''] }, rate_limiter_options)
+    Rack::RateLimiter.new(lambda { |env| [200, {}, ''] }, rate_limiter_options, &rake_limiter_customization_block)
   end
 
   it 'returns a successful response' do
@@ -123,6 +124,35 @@ describe Rack::RateLimiter do
         Timecop.freeze(current_time_user_2)
         get '/', {}, 'REMOTE_ADDR' => ip_user_2
         expect(last_response.header['X-RateLimit-Reset']).to eq(time_after_an_hour(current_time_user_2).to_i)
+      end
+    end
+  end
+
+  describe  'custom user detection' do
+    describe 'with an additional block' do
+      let(:rake_limiter_customization_block) { Proc.new { |env| Rack::Request.new(env).params['API_TOKEN'] } }
+
+      it 'identifies users by the token' do
+        get '/', {'API_TOKEN' => 'ighVrvNmkLvWmjlFUZHzYQ'}, 'REMOTE_ADDR' => '172.16.1.1'
+        get '/', {'API_TOKEN' => 'ighVrvNmkLvWmjlFUZHzYQ'}, 'REMOTE_ADDR' => '172.16.1.2'
+        expect(last_response.header['X-RateLimit-Remaining']).to eq(58)
+      end
+
+      it 'does not include X-RateLimit-Limit, X-RateLimit-Remaining and X-RateLimit-Reset if the block return nil' do
+        get '/'
+        expect(last_response.header).not_to include('X-RateLimit-Limit')
+        expect(last_response.header).not_to include('X-RateLimit-Remaining')
+        expect(last_response.header).not_to include('X-RateLimit-Reset')
+      end
+    end
+
+    describe 'without an additional block' do
+      it 'identifies users by their address IP' do
+        2.times { get '/', {}, 'REMOTE_ADDR' => '172.16.1.1' }
+        expect(last_response.header['X-RateLimit-Remaining']).to eq(58)
+
+        5.times {get '/', {}, 'REMOTE_ADDR' => '172.16.1.2' }
+        expect(last_response.header['X-RateLimit-Remaining']).to eq(55)
       end
     end
   end
